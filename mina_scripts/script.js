aa177eeea068b4602c66ef9a6563637fffac5cb1
Sebastian Salonen <sebastian.salonen@integpro.se>
2022-12-14 21:20:05 +0100

    - updates from 13/12

--- mina_scripts/script.js
+++ mina_scripts/script.js
@@ -1,17 +1,31 @@
+let classListReference = [];
+
+function filterStudents(filterValue)
+{
+    let classListElements = document.querySelectorAll(".class-list-item");
+    classListElements.forEach((listItem) => {
+
+        const itemName = listItem.querySelector("#class-list-item-name").innerText;
+        if(itemName.indexOf(filterValue) < 0)
+        {
+            listItem.classList.add("hide");
+        }
+        else
+        {
+            listItem.classList.remove("hide");
+        }
+
+    });
+}
 
 
-function createStudent()
+function createStudent(inputName, inputPhone, inputYears)
 {
     // Håller hårdkodade värden samlat vid toppen istället 
     // för att sprida ut dessa i koden/funktionen så andra
     // utvecklare inte behöver leta dessa (eller när du kommer 
     // tillbaka till koden efter ett längre tag).
     const btnText = "Ta bort";
-
-    // Hämtar värden från våra input taggar
-    let inputName = document.querySelector("#inputName").value;
-    let inputPhone = document.querySelector("#inputPhone").value;
-    let inputYears = document.querySelector("#inputYears").value;
     
     // Hämtar referens till <div class="class-list">
     let classListEl = document.querySelector(".class-list");
@@ -25,12 +39,15 @@ function createStudent()
     // Skapar elementen som ska vara i klassraden:
     // <span>inputName</span>
     let classListItemName = document.createElement("span");
+    classListItemName.id = "class-list-item-name";
     classListItemName.innerText = inputName;
     // <span>inputPhone</span>
     let classListItemPhone = document.createElement("span");
+    classListItemPhone.id = "class-list-item-phone";
     classListItemPhone.innerText = inputPhone;
     // <span>inputYears</span>
     let classListItemYears = document.createElement("span");
+    classListItemYears.id = "class-list-item-years";
     classListItemYears.innerText = inputYears;
     // <button onclick="removeStudent()">btnText</button>
     let classListItemRemoveBtn = document.createElement("button");
